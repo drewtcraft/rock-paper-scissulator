cfcba3ee5489c9fa303b9f1d847e6f58eaa359f7
Andrew Craft <drewtcraft@gmail.com>
2023-05-07 08:44:41 -0400

    success! v1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use bevy::{prelude::*, window::PrimaryWindow, render::view::window};
+use bevy::{prelude::*, window::PrimaryWindow};
 use rand::prelude::*;
-use std::collections::HashMap;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+
         .add_startup_system(spawn_camera_ssys)
         .add_startup_system(spawn_rocks_papers_scissors_ssys)
-        // .add_system(rock_movement_sys)
-        // .add_system(paper_movement_sys)
-        // .add_system(scissors_movement_sys)
+
         .add_system(movement_sys::<Rock, Paper, Scissors>)
+        .add_system(movement_sys::<Paper, Scissors, Rock>)
+        .add_system(movement_sys::<Scissors, Rock, Paper>)
         .add_system(contain_entities_sys)
+        .add_system(detect_collisions_from_predators_sys::<Rock, Paper>)
+        .add_system(detect_collisions_from_predators_sys::<Paper, Scissors>)
+        .add_system(detect_collisions_from_predators_sys::<Scissors, Rock>)
+
         .run();
 }
 
@@ -20,21 +24,39 @@ pub const ENTITY_SPEED: f32 = 100.0;
 pub const ENTITY_SIZE: f32 = 64.0;
 pub const ENTITY_HALF_SIZE: f32 = 32.0;
 
-#[derive(Component)]
+pub trait AssociatedString {
+    const STRING: &'static str;
+}
+
+#[derive(Component, Default)]
 pub struct Rock;
 
-#[derive(Component)]
+impl AssociatedString for Rock {
+    const STRING: &'static str = "rock";
+}
+
+#[derive(Component, Default)]
 pub struct Paper;
 
-#[derive(Component)]
+impl AssociatedString for Paper {
+    const STRING: &'static str = "paper";
+}
+
+#[derive(Component, Default)]
 pub struct Scissors;
 
+impl AssociatedString for Scissors {
+    const STRING: &'static str = "scissors";
+}
+
+// TODO get rid of this thing
 pub enum RoShamBo {
     Rock,
     Paper,
     Scissors,
 }
 
+// all Rock, Paper, or Scissors bundles will include this component
 #[derive(Component)]
 pub struct IsInFoodChain;
 
@@ -56,10 +78,13 @@ pub fn spawn_camera_ssys(
 ) {
     let window = window_query.get_single()
         .expect("There is no window, how is this possible?");
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 7.0),
-        ..default()
-    });
+
+    commands.spawn(
+        Camera2dBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 7.0),
+            ..default()
+        }
+    );
 }
 
 pub fn spawn_rocks_papers_scissors_ssys(
@@ -78,6 +103,7 @@ pub fn spawn_rocks_papers_scissors_ssys(
     }
 }
 
+// TODO make generic
 pub fn spawn_entity(
     commands: &mut Commands,
     window: &Window, 
@@ -117,109 +143,19 @@ pub fn generate_random_transform(window: &Window) -> Transform {
     Transform::from_xyz(random_x, random_y, 0.0)
 }
 
-// can I throw <T, M, N, L> on here and specify the types that way??
+// this is cool but might just make more sense to do in a single loop
 pub fn movement_sys<O: Component, H: Component, L: Component>(
     mut own_query: Query<&mut Transform, With<O>>,
     predators_query: Query<&Transform, (With<H>, Without<O>)>,
     prey_query: Query<&Transform, (With<L>, Without<O>)>,
     time: Res<Time>
 ) {
-    let predator_positions: Vec<&Vec3> = predators_query.iter()
-        .map(|t| { &t.translation })
+    let predator_positions: Vec<Vec3> = predators_query.iter()
+        .map(|t| { t.translation })
         .collect();
 
-    let prey_positions: Vec<&Vec3> = prey_query.iter()
-        .map(|t| {&t.translation })
-        .collect();
-
-    for mut transform in own_query.iter_mut() {
-        let direction = get_own_direction(
-            &transform, 
-            predator_positions.clone(), 
-            prey_positions.clone(),
-        );
-
-        if let Some(mut direction) = direction {
-            if direction.length() > 0.0 {
-                direction = direction.normalize();
-            }
-            transform.translation += direction * ENTITY_SPEED * time.delta_seconds();
-        }
-    }
-}
-
-pub fn rock_movement_sys(
-    mut own_query: Query<&mut Transform, With<Rock>>,
-    predators_query: Query<&Transform, (With<Paper>, Without<Rock>)>,
-    prey_query: Query<&Transform, (With<Scissors>, Without<Rock>)>,
-    time: Res<Time>
-) {
-    let predator_positions: Vec<&Vec3> = predators_query.iter()
-        .map(|t| { &t.translation })
-        .collect();
-
-    let prey_positions: Vec<&Vec3> = prey_query.iter()
-        .map(|t| {&t.translation })
-        .collect();
-
-    for mut transform in own_query.iter_mut() {
-        let direction = get_own_direction(
-            &transform, 
-            predator_positions.clone(), 
-            prey_positions.clone(),
-        );
-
-        if let Some(mut direction) = direction {
-            if direction.length() > 0.0 {
-                direction = direction.normalize();
-            }
-            transform.translation += direction * ENTITY_SPEED * time.delta_seconds();
-        }
-    }
-}
-
-pub fn paper_movement_sys(
-    mut own_query: Query<&mut Transform, With<Paper>>,
-    predators_query: Query<&Transform, (With<Rock>, Without<Paper>)>,
-    prey_query: Query<&Transform, (With<Scissors>, Without<Paper>)>,
-    time: Res<Time>
-) {
-    let predator_positions: Vec<&Vec3> = predators_query.iter()
-        .map(|t| { &t.translation })
-        .collect();
-
-    let prey_positions: Vec<&Vec3> = prey_query.iter()
-        .map(|t| {&t.translation })
-        .collect();
-
-    for mut transform in own_query.iter_mut() {
-        let direction = get_own_direction(
-            &transform, 
-            predator_positions.clone(), 
-            prey_positions.clone(),
-        );
-
-        if let Some(mut direction) = direction {
-            if direction.length() > 0.0 {
-                direction = direction.normalize();
-            }
-            transform.translation += direction * ENTITY_SPEED * time.delta_seconds();
-        }
-    }
-}
-
-pub fn scissors_movement_sys(
-    mut own_query: Query<&mut Transform, With<Scissors>>,
-    predators_query: Query<&Transform, (With<Rock>, Without<Scissors>)>,
-    prey_query: Query<&Transform, (With<Paper>, Without<Scissors>)>,
-    time: Res<Time>
-) {
-    let predator_positions: Vec<&Vec3> = predators_query.iter()
-        .map(|t| { &t.translation })
-        .collect();
-
-    let prey_positions: Vec<&Vec3> = prey_query.iter()
-        .map(|t| {&t.translation })
+    let prey_positions: Vec<Vec3> = prey_query.iter()
+        .map(|t| {t.translation })
         .collect();
 
     for mut transform in own_query.iter_mut() {
@@ -240,35 +176,13 @@ pub fn scissors_movement_sys(
 
 pub fn get_own_direction(
     transform: &Mut<Transform>, 
-    predator_positions: Vec<&Vec3>, 
-    prey_positions: Vec<&Vec3>
+    predator_positions: Vec<Vec3>, 
+    prey_positions: Vec<Vec3>
 ) -> Option<Vec3> {
     let translation = transform.translation;
-    let closest_predator_position: Option<&Vec3> = predator_positions.iter()
-        .fold(None, |acc, t| { 
-            if let Some(existing) = acc {
-                if existing.distance(translation) > t.distance(translation) { 
-                    Some(t)
-                } else { 
-                    acc
-                }
-            } else {
-                Some(t)
-            }
-        });
 
-    let closest_prey_position: Option<&Vec3> = prey_positions.iter()
-        .fold(None, |acc, t| {
-            if let Some(existing) = acc {
-                if existing.distance(translation) > t.distance(translation) { 
-                    Some(t)
-                } else { 
-                    acc
-                }
-            } else {
-                Some(t)
-            }
-        });
+    let closest_predator_position: Option<Vec3> = get_closest(translation, &predator_positions);
+    let closest_prey_position: Option<Vec3> = get_closest(translation, &prey_positions);
 
     let run_away = if closest_predator_position.is_some() && closest_prey_position.is_some() {
         closest_predator_position.unwrap().distance(translation) < closest_prey_position.unwrap().distance(translation)
@@ -299,6 +213,21 @@ pub fn get_own_direction(
     Some(direction)
 }
 
+pub fn get_closest(target: Vec3, positions: &Vec<Vec3>) -> Option<Vec3> {
+    positions.iter()
+        .fold(None, |acc, t| { 
+            if let Some(existing) = acc {
+                if existing.distance(target) > t.distance(target) { 
+                    Some(*t)
+                } else { 
+                    acc
+                }
+            } else {
+                Some(*t)
+            }
+        })
+}
+
 pub fn contain_entities_sys(
     mut entities_query: Query<&mut Transform, With<IsInFoodChain>>,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -319,6 +248,39 @@ pub fn contain_entities_sys(
             e.translation.y = y_max;
         } else if e.translation.y > y_max {
             e.translation.y = y_min;
+        }
+    }
+}
+
+pub fn detect_collisions_from_predators_sys<O: Component, H: Component + Default + AssociatedString>(
+    mut commands: Commands,
+    mut own_query: Query<(&Transform, Entity), With<O>>,
+    predators_query: Query<&Transform, (With<H>, Without<O>)>,
+    asset_server: Res<AssetServer>
+) {
+    let predator_positions: Vec<Vec3> = predators_query.iter()
+        .map(|t| { t.translation })
+        .collect();
+
+    for (transform, entity) in own_query.iter_mut() {
+        let translation = transform.translation;
+
+        let closest_predator_position: Option<Vec3> = get_closest(translation, &predator_positions);
+
+        if let Some(closest_predator_position) = closest_predator_position {
+            if closest_predator_position.distance(translation) < ENTITY_SIZE {
+                commands.entity(entity)
+                    .remove::<O>()
+                    .remove::<SpriteBundle>()
+                    .insert((
+                        H::default(),
+                        SpriteBundle {
+                            transform: *transform,
+                            texture: asset_server.load(format!("sprites/{}.png", H::STRING)),
+                            ..default()
+                        }
+                    ));
+            }
         }
     }
 }
